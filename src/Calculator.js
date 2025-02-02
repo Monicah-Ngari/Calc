001e26660186bf26d5c2ed93ad70fd7ef2848ade
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-02 12:59:46 +0300

    installed babel

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,33 +1,36 @@
 import "./Calculator .css";
 import Calcbutton from "./Calcbutton";
 
-function Calculator(props) {
+function Calculator() {
   return (
     <div className="Calculator">
       <div className="container">
         <div>
+          <Calcbutton className="display" value={0} />
+        </div>
+        <div>
           <Calcbutton value={7} />
           <Calcbutton value={8} />
           <Calcbutton value={9} />
-          <Calcbutton value={"Del"} />
+          <Calcbutton className="operator" value={"Del"} />
         </div>
         <div>
           <Calcbutton value={4} />
           <Calcbutton value={5} />
           <Calcbutton value={6} />
-          <Calcbutton value={"*"} />
+          <Calcbutton className="operator" value={"*"} />
         </div>
         <div>
           <Calcbutton value={1} />
           <Calcbutton value={2} />
           <Calcbutton value={3} />
-          <Calcbutton value={"+"} />
+          <Calcbutton className="operator" value={"+"} />
         </div>
         <div>
           <Calcbutton value={"-"} />
           <Calcbutton value={0} />
           <Calcbutton value={"%"} />
-          <Calcbutton value={"="} />
+          <Calcbutton className="operator" value={"="} />
         </div>
       </div>
     </div>

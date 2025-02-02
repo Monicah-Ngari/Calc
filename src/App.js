287e91c5c892732f5296af79ee4699adfae902d6
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-02 09:59:40 +0300

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
 
 function Calculator() {
-  return <div className="Calculator"></div>;
+  return (
+    <div className="Calculator">
+      <div className="container">
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;

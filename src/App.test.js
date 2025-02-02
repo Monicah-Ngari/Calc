dfb81ca909711a289efa60b8f87acb120f7313e6
Monicah-Ngari <wakarimangari@gmail.com>
2025-02-02 19:47:57 +0300

    added css to my calculator

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
+import Calculator from "./Calculator";
+
+test("renders learn react link", () => {
+  render(<Calculator />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,37 +1,31 @@
-import "./Calculator .css";
+import "./Calculator.css";
 import Calcbutton from "./Calcbutton";
 
 function Calculator() {
   return (
     <div className="Calculator">
-      <div className="container">
-        <div>
-          <Calcbutton className="display" value={0} />
-        </div>
-        <div>
-          <Calcbutton value={7} />
-          <Calcbutton value={8} />
-          <Calcbutton value={9} />
-          <Calcbutton className="operator" value={"Del"} />
-        </div>
-        <div>
-          <Calcbutton value={4} />
-          <Calcbutton value={5} />
-          <Calcbutton value={6} />
-          <Calcbutton className="operator" value={"*"} />
-        </div>
-        <div>
-          <Calcbutton value={1} />
-          <Calcbutton value={2} />
-          <Calcbutton value={3} />
-          <Calcbutton className="operator" value={"+"} />
-        </div>
-        <div>
-          <Calcbutton value={"-"} />
-          <Calcbutton value={0} />
-          <Calcbutton value={"%"} />
-          <Calcbutton className="operator" value={"="} />
-        </div>
+      <div className="Calculator-container">
+        <div className="Calculator-display">0 </div>
+
+        <Calcbutton value={7} />
+        <Calcbutton value={8} />
+        <Calcbutton value={9} />
+        <Calcbutton className="operator" value={"Del"} />
+
+        <Calcbutton value={4} />
+        <Calcbutton value={5} />
+        <Calcbutton value={6} />
+        <Calcbutton className="operator" value={"*"} />
+
+        <Calcbutton value={1} />
+        <Calcbutton value={2} />
+        <Calcbutton value={3} />
+        <Calcbutton className="operator" value={"+"} />
+
+        <Calcbutton value={"-"} />
+        <Calcbutton value={0} />
+        <Calcbutton value={"%"} />
+        <Calcbutton className="operator" value={"="} />
       </div>
     </div>
   );
